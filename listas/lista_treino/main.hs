d99fb17ac570e41572b528d9f7e072ee1f886f13
Lucas Henrique <hns.lucass@gmail.com>
2024-06-23 00:37:38 -0300

    feat: add diagPrincipal and Stack

--- listas/lista_treino/main.hs
+++ listas/lista_treino/main.hs
@@ -6,10 +6,12 @@
 --               t         u   
 -- map . (.) :: (b -> c) -> ([a -> b]-> [a -> c])
 
+-- Questao 2 
 sublistas :: [a] -> [[a]]
 sublistas [] = [[]]
 sublistas (x:xs) = [x:ys | ys <- sublistas xs] ++ sublistas xs
 
+-- Questao 3 
 poli :: Integer -> Integer -> Integer -> Integer -> Integer 
 poli a b c = \x -> a*x*x + b*x + c
 
@@ -21,6 +23,7 @@ appListaPoli [] [] = []
 appListaPoli (x:xs) (y:ys) = x y: appListaPoli xs ys
 appListaPoli lPoli lInt = [f x | (f,x) <- zip lPoli lInt]
 
+-- Questao 4
 matriz :: [[Integer]]
 matriz = [[1,2,3,4,5,6],[7,8,9,10,11]]
 
@@ -39,7 +42,37 @@ permutaValores :: Int -> Int -> [a] -> [a]
 permutaValores x y m =
     init (take x m) ++ 
     [m !! (y-1)] ++
-    (take (y-x-1) (drop x m)) ++
+    take (y-x-1) (drop x m) ++
     [m !! (x-1)] ++
-    (drop y m)
+    drop y m
 
+extraiPrincipal :: [[Integer]] -> Int -> [Integer]
+extraiPrincipal l 0 = [(l!!0)!!0]
+extraiPrincipal l i = (l!!i)!!i : extraiPrincipal l (i-1)
+
+-- diagPrincipal :: [[Integer]] -> [Integer]
+diagPrincipal :: [[Integer]] -> [Integer]
+diagPrincipal l = reverse (extraiPrincipal l((length l)-1))
+
+-- Questao 5 
+
+
+
+
+
+
+-- Questao 6
+data Pilha t = Pilha [t] | PilhaVazia
+            deriving(Show)
+
+push :: t -> Pilha t -> Pilha t
+push x PilhaVazia = Pilha [x]
+push x (Pilha t) = Pilha (x:t)
+
+pop :: Pilha t -> Pilha t 
+pop PilhaVazia  = error "Pilha Vazia"
+pop (Pilha (x:xs)) = Pilha xs  
+
+top :: Pilha t -> t 
+top PilhaVazia = error "Pilha Vazia"
+top (Pilha (x:xs)) = x 
